dfaf7b295bb0568f884fe58ccbbe7d4aa59c9123
Sasha Semenishchev <sashasemenishchev@gmail.com>
2021-08-23 16:47:04 +0300

    Initial commit

---
+++ src/main/java/me/mrfunny/plugins/spigot/survpostotr/Survpostor.java
@@ -0,0 +1,17 @@
+package me.mrfunny.plugins.spigot.survpostotr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Survpostor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
